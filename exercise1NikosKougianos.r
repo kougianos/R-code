bcaae3397a068913442ef519402bb0de7a63328a
Nikos Kougianos <kougi_jr@hotmail.com>
2020-11-27 19:23:29 +0200

    new r code

---
+++ exercise1NikosKougianos.r
@@ -0,0 +1,50 @@
+func1 <- function(x) {
+    
+    result <- c()
+    
+    for(i in 1:length(x)) {
+
+        result<-append(result, x[i]**i)
+    }
+    return(result)
+    
+}
+
+func2 <- function(x) {
+    
+    result <- c()
+    
+    for(i in 1:length(x)) {
+        
+        result<-append(result, (x[i]**i)/i)
+    }
+    return(result)
+    
+}
+
+func3 <- function(x, n) {
+    
+    if(n<=0) {
+        return("n must be a positive number")
+    }
+    
+    result = 1
+    
+    for(i in 1:n) {
+        result = result + (x**i)/i
+    }
+    
+    return(result)
+    
+}
+
+func1(c(2,3,4,5,7,9))
+
+func2(c(4,3,5,2))
+
+func3(3,3)
+
+
+
+
+
