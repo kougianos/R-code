022d1daa0db5c6490b956d93b1855f3937b0631c
Nikos Kougianos <kougi_jr@hotmail.com>
2021-01-16 18:52:10 +0200

    more dplyr commands

--- test.r
+++ test.r
@@ -9,3 +9,18 @@ class(airquality)
 airquality
 
 select(airquality, Ozone, Solar.R, Day)
+
+filter(airquality, Month >5, Month < 9, Day <3)
+
+filter(airquality, Day ==1 | Day ==2)
+
+# Sort by Ozone (asc by default) first , then by Solar.R
+arrange(airquality, Ozone, desc(Solar.R))
+
+mutate(airquality, Temp.C = round((Temp -32)*5/9))
+
+# pipe operator, square root of 9
+9 %>% sqrt()
+
+?starts_with
+
